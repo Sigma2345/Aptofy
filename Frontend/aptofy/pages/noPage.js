3c113cb4e47132caa9c4bcc88a140b01a8ce7d27
shisuimadara <2020ucs0090@iitjammu.ac.in>
2023-12-07 16:24:00 +0530

    added upload, request and song upload home page and profile

---
+++ Frontend/aptofy/pages/noPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./home.module.css";
+
+export const NoPage = () => {
+  return (
+    <div
+      class={`${styles.container} h-screen flex items-center justify-center`}
+    >
+      <div
+        class={`${styles.bgGray} h-screen flex flex-col items-center justify-center`}
+      >
+        <div class="tracking-widest mt-4 text-center">
+          <span class="text-gray-500 text-6xl block">
+            <span>4 0 4</span>
+          </span>
+          <span class="text-gray-500 text-xl">
+            Sorry, We couldn't find what you are looking for!
+          </span>
+        </div>
+        <div class="mt-6">
+          <a
+            href="/"
+            class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md"
+          >
+            Go back
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};

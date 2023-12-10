cebd45c33dcff0d4fb58a6ca624e381c550d50f6
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-12-10 16:08:16 +0530

    env from next.config.js -> .env.local

--- Frontend/aptofy/next.config.js
+++ Frontend/aptofy/next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    env: {
-        mongo_url: "mongodb://localhost:27017",
-        db_name: "aptofy_t69"
-    }
 };
 
 module.exports = nextConfig;

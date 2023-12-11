381bb7ba34ae87c755be2f7c076750eb6da53313
Sigma2345 <mudit1978mars@gmail.com>
2023-12-11 22:53:11 +0530

    updated README

--- Frontend/aptofy/common/constants.js
+++ Frontend/aptofy/common/constants.js
@@ -1,27 +1,15 @@
-export const MODULE_ADDRESS = "0x466b9957636118d2d8b333531cafb3e696ebb8d608543a4d7bb769bddbb650dd"
+export const MODULE_ADDRESS = "0x764ba2d5b1aa23aa776c292a81c76ccdca46bf1d96f056761b52a9383f0083c3"
 export const FAQs = [
     {
         question: "How do I register?",
-        answer: "Make an account on Aptos wallet and simply login.",
+        answer: "Connect your petra wallet to the website and you are in.",
     },
     {
         question: "Are there any hidden charges?",
-        answer: "There are no hidden charges. The song registration is entirely free of cost.",
+        answer: "There are no hidden charges. Only the network fees for registration and 5% of donations are charged to keep the platform running.",
     },
     {
-        question: "Who will accept my upload request?",
-        answer: "The system will check and respond the request.",
-    },
-    {
-        question: "How much time is taken to respond?",
-        answer: "It generally happens in hours but can take 1-2 days too.",
-    },
-    {
-        question: "Any fraud checks?",
-        answer: "The system checks and filter frauds and duplicates",
-    },
-    {
-        question: "More",
-        answer: "Yes",
-    },
+        question: "Does the platform bias towards any artist?",
+        answer: "No, the platform is completely unbiaased"
+    }, 
 ];

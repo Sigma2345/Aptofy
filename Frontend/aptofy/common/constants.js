9f5234a2a602df34160967384bc19ea3a49a4b33
Sigma2345 <mudit1978mars@gmail.com>
2023-12-11 20:06:38 +0530

    chnaged FAQs and debugging

--- Frontend/aptofy/common/constants.js
+++ Frontend/aptofy/common/constants.js
@@ -1,3 +1,27 @@
-const MODULE_ADDRESS = "0x466b9957636118d2d8b333531cafb3e696ebb8d608543a4d7bb769bddbb650dd"
-
-export default MODULE_ADDRESS; 
+export const MODULE_ADDRESS = "0x466b9957636118d2d8b333531cafb3e696ebb8d608543a4d7bb769bddbb650dd"
+export const FAQs = [
+    {
+        question: "How do I register?",
+        answer: "Make an account on Aptos wallet and simply login.",
+    },
+    {
+        question: "Are there any hidden charges?",
+        answer: "There are no hidden charges. The song registration is entirely free of cost.",
+    },
+    {
+        question: "Who will accept my upload request?",
+        answer: "The system will check and respond the request.",
+    },
+    {
+        question: "How much time is taken to respond?",
+        answer: "It generally happens in hours but can take 1-2 days too.",
+    },
+    {
+        question: "Any fraud checks?",
+        answer: "The system checks and filter frauds and duplicates",
+    },
+    {
+        question: "More",
+        answer: "Yes",
+    },
+];
